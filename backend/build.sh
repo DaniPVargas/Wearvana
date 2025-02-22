fda9382364f444bdcc0a71e3f0413b2dad33774b
sergio-alv-per <sergioalvarezperez00@gmail.com>
2025-02-22 21:29:25 +0100

    playwright.

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,2 @@
 pip install -r requirements.txt
-wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
-tar -xzvf geckodriver-v0.35.0-linux64.tar.gz
-sudo cp geckodriver /usr/local/bin/geckodriver
+playwright install-deps
