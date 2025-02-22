450f918efa2cd80c87ea840d58551458e4a8ee40
sergio-alv-per <sergioalvarezperez00@gmail.com>
2025-02-22 23:26:40 +0100

    update test.

--- backend/test_playwright.py
+++ backend/test_playwright.py
@@ -1,8 +1,13 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.webkit.launch()
+    browser = p.chromium.launch()
     page = browser.new_page()
-    page.goto("https://playwright.dev/")
-    page.screenshot(path="example.png")
+    page.goto("https://www.zara.com/es/en/zw-collection-ruffled-shirt-p09479040.html?v1=441075492")
+    first_image = page.locator('img').first()
+
+    # Optionally, get the src attribute or other properties of the image
+    src = first_image.get_attribute('src')
+    print(src)
+
     browser.close()
