fd90d1fdac4c8f283da248bbddf26a5637e91ca3
sergio-alv-per <sergioalvarezperez00@gmail.com>
2025-02-22 21:17:33 +0100

    update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,4 @@
 pip install -r requirements.txt
-cp /var/pictures/geckodriver /usr/local/bin/geckodriver
+wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
+tar -xzvf geckodriver-v0.35.0-linux64.tar.gz
+cp geckodriver /usr/local/bin/geckodriver
